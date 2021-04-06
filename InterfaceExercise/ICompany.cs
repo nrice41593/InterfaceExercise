e60f3ee4dc7764e0cfe7d204e87cf4219c8c2ca6
IVProb <jnoojinjr@gmail.com>
2021-04-06 08:04:47 -0500

    Need to print

--- InterfaceExercise/ICompany.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace InterfaceExercise
-{
-    public interface ICompany
-    {
-        public string CompanyName { get; set; }
-        public string Motto { get; set; }
-    }
-}
